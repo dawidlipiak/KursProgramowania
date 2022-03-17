86c408042e6002f151139fe1ab0fd84173d6b93c
dawidlipiak <91973266+dawidlipiak@users.noreply.github.com>
2022-03-17 10:07:02 +0100

    Update Test.cpp

--- Kurs Programowania/Lista 2/Test.cpp
+++ Kurs Programowania/Lista 2/Test.cpp
@@ -1,47 +1,10 @@
+//main Test.cpp
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
+#include <LiczbyPierwsze.cpp>
 
-
-class LiczbyPierwsze{
-    int* tabPierwsze;
-    
-    public:
-    LiczbyPierwsze(int n){
-        
-        tabPierwsze = (int*) malloc(n*sizeof(int));
-        
-        int licznik = 0;
-        bool czyPierwsza;
-
-        for(int i = 2; i <= n; i++){
-
-            czyPierwsza = true;
-
-            for(int j = 2; j<i ; j++) {
-                 if (i%j == 0) {
-                    czyPierwsza = false;
-                    break;
-                }
-            }
-
-            if(czyPierwsza == true){
-                tabPierwsze[licznik] = i;
-                licznik++;
-            }
-        }
-
-    }
-    int liczba (int m){
-        return tabPierwsze[m];
-    }
-    
-    ~LiczbyPierwsze(){
-        free(tabPierwsze);
-    }
-
-};
 
 int main (int argc, char *argv[])
 {

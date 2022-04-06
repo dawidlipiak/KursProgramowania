2550f017217dc72808283a34f14313d8721d09df
dawidlipiak <91973266+dawidlipiak@users.noreply.github.com>
2022-04-06 02:27:44 +0200

    lista4

---
+++ Kurs Programowania/lista4/Figura.java
@@ -0,0 +1,11 @@
+public abstract class Figura implements Obliczenia { 
+    @Override
+    public double obliczObwod() {
+        return 0;
+    }
+
+    @Override
+    public double obliczPole() {
+        return 0;
+    }
+}
